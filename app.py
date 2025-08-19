930fe8781eac0e8302bdaa3476cb42bfe3f5f9a8
MatiasJavierLucero <43035213+matiasjavierlucero@users.noreply.github.com>
2025-08-19 17:44:09 -0300

    sin variables de entorno

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://usuario@host:6000/la_base"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
